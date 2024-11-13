48e6d2aa17a75e59d7241e9ffcd5644fd40a27d4
KirillLomtev <k.lomtev.01@gmail.com>
2024-11-13 11:27:38 +0300

    Update module_3_task_1.py

--- module_3_task_1.py
+++ module_3_task_1.py
@@ -2,22 +2,22 @@ calls = 0
 def count_calls():
     global calls
     calls+=1
-def string_info():
-    a = input()
+
+def string_info(a):
     tuple_a = (len(a), a.upper(), a.lower())
     print(tuple_a)
     count_calls()
     return(tuple_a)
-def is_contains():
-    string = input().strip()
-    list_to_search = input().strip().split(',')
+
+def is_contains(string, list_to_search):
     list_to_search = [item.strip().lower() for item in list_to_search]
     string = string.lower()
     count_calls()
     return string in list_to_search
 
 
-string_info()
-resultat = is_contains()
-print(resultat)
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
+print(is_contains('cycle', ['recycling', 'cyclic']))
 print(calls)
